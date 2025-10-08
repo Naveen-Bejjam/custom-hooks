85eccef6184b28548aba652fcde1cdb34b2b1015
Naveen Bejjam <naveen@Naveens-MacBook-Pro.local>
2025-10-08 15:51:51 +0530

    test coverage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Search from './components/Search';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <Search/>
+      <button onClick={()=>setCount((prev)=>prev+1)}>increment</button>
+      <div>learn react</div>
+      <div>naveen</div>
+      <h1>{count}</h1>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("render naveen", ()=>{
+  render(<App/>);
+  const element = screen.getByText("naveen");
+  expect(element).toBeInTheDocument();
+})
+
+test("click event", async ()=>{
+  render(<App/>);
+  const buttonElement = screen.getByRole("button", {
+    name : "increment"
+  });
+  
+  expect(buttonElement).toBeInTheDocument();
+  await userEvent.click(buttonElement);
+  const countElement = screen.getByRole("heading");
+  expect(countElement).toHaveTextContent("1")
+})
